a6b53f87d0fbe904476c5383c7460603da3c9732
Wong Tsz Him <mariowong2011@gmail.com>
2025-05-10 03:06:12 +0800

    implement search controller

--- otps-test/src/Backend/searchController.cjs
+++ otps-test/src/Backend/searchController.cjs
@@ -1,7 +1,24 @@
 const { db } = require('./firebase.cjs');
 
 const searchController = {
+    searchRelevantName: async (req, res) => {
+        const { name } = req.body;
+        try {
+            const userRef = db.collection('student');
+            const snapshot = await userRef.get(); // Fetch all documents
+            const matchingRecords = [];
 
+            snapshot.forEach(doc => {
+                const studentInfo = doc.data().studentInfo;
+                if (studentInfo && (studentInfo.username.toLowerCase().includes(name.toLowerCase()) || studentInfo.name.toLowerCase().includes(name.toLowerCase()))) 
+                    matchingRecords.push(studentInfo);
+            });
+          return res.json({matchingRecords});
+        } catch (error) {
+          console.error('Error checking login information:', error);
+          res.status(500).json({ error: 'Server error' });
+        }
+    },
 };
 
 module.exports = searchController;
